ba68cdfba7bc3f94b3d8da658c00ae9ab811ddae
Qingshuai Tian <qingshuai.tian@jaltone.com>
2023-01-31 14:14:23 +0800

    Add SpringBoot Framework

---
+++ src/main/java/com/tianqingshuai/gitlabwxworkwebhookadapter/GitlabWxworkWebhookAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.tianqingshuai.gitlabwxworkwebhookadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabWxworkWebhookAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitlabWxworkWebhookAdapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tianqingshuai/gitlabwxworkwebhookadapter/GitlabWxworkWebhookAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianqingshuai.gitlabwxworkwebhookadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabWxworkWebhookAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
